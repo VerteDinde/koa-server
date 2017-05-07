e52a31b813ac268d4e8d31c45579edd969274def
Keeley Hammond <keeleymhammond@gmail.com>
2017-05-06 17:47:00 -0700

    Hahaha, I did all of that work and got the exact same error, super cool.

--- test/poms.test.js
+++ test/poms.test.js
@@ -1,13 +1,10 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../lib/app');
-
-const assert = chai.assert;
-chai.use(chaiHttp);
+const db = require('./_db');
+const request = require('./_request');
+const assert = require('chai').assert;
 
 describe('Poms Koa API', () => {
 
-  const request = chai.request(app); //why does this need to be inside 'describe'?
+  before(db.drop);
 
   it('GET/ all poms', () => {
     return request.get('api/poms')
