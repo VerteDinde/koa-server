917004bb5db22577c9547708c7b35465b04fcaa6
Keeley Hammond <keeleymhammond@gmail.com>
2017-05-06 17:06:27 -0700

    I... think it's working?

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const app = require('./lib/app');
 const http = require('http');
 require('./lib/connect');
 
-app.listen(3000, () => {
-  console.log('Server listening at PORT 3000');
-});
+// app.listen(3000, () => {
+//   console.log('Server listening at PORT 3000');
+// });
+
+const server = http.createServer(app.callback());
+server.listen(3000);
